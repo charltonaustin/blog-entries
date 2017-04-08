8333a89c421bed9ce01659c9a1631d9494ebada8
Charlton Austin <charlton.austin@gmail.com>
2017-04-08 09:31:56 -0400

    Making publishing a little bit easier.

--- publish_blog_post.sh
+++ publish_blog_post.sh
@@ -1,1 +1,6 @@
-mkdir -p published/`date +%Y/%m/%d` && mv $1 published/`date +%Y/%m/%d`/
+set -ex
+DATE=`date +%Y/%m/%d`
+NAME=`echo $1 | sed "s/^unpublished\///"`
+mkdir -p published/$DATE && mv $1 published/$DATE/
+git add $1 published/$DATE/$NAME
+git commit -m "Pubslishing new blog post published/$DATE/$NAME"
