36766a5076b161a229a5d7a90d3e8fe2afcdef62
charltonaustin <charlton.austin@gmail.com>
2018-07-23 15:10:36 -0400

    Fixing the structure so I remember what is ready for publishing and what isn't

--- publish_blog_post.sh
+++ publish_blog_post.sh
@@ -1,6 +1,7 @@
+#!/usr/bin/env bash
 set -ex
 DATE=`date +%Y/%m/%d`
-NAME=`echo $1 | sed "s/^unpublished\///"`
-mkdir -p published/$DATE && mv $1 published/$DATE/
-git add $1 published/$DATE/$NAME
-git commit -m "Pubslishing new blog post published/$DATE/$NAME"
+NAME=`echo $1 | sed "s/^ready_to_publish\///"`
+mkdir -p published/${DATE} && mv $1 published/${DATE}/
+git add $1 published/${DATE}/${NAME}
+git commit -m "Publishing new blog post published/$DATE/$NAME"
